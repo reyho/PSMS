342483b0e025675b14d0240b3ef0803763a7bed8
Rejhan Mujkanović <rejhom@gmail.com>
2018-09-29 00:07:40 +0200

    Changed names of variables
    I changed the names that contained HTTP to HTTPS, nothing special.

--- client script/PSMSP/Downloader.py
+++ client script/PSMSP/Downloader.py
@@ -7,31 +7,31 @@ from clint.textui import progress
 import re
 
 class Downloader:
-    
-    #Initialising the object with the decrypted response, request object, url, and error detection
+
+    #Initialising the object with the decrypted response, request object and url
     def __init__(self, decrypted_response_request_url_error):
-        self.__decrypted_HTTP_response = decrypted_response_request_url_error["decrypted_response"]
+        self.__decrypted_HTTPS_response = decrypted_response_request_url_error["decrypted_response"]
         self.__request = decrypted_response_request_url_error["request"]
         self.__url = decrypted_response_request_url_error["url"]
-    
-    #Sending the decrypted response to the server and downloading the encrypted file    
-    def download_encrypted_file(self): 
+
+    #Sending the decrypted response to the server and downloading the encrypted file
+    def download_encrypted_file(self):
         try:
-            HTTP_response = self.__request.get(self.__url, params={'value': self.__decrypted_HTTP_response, 'mode': 'DOWNLOAD'}, stream=True)
+            HTTPS_response = self.__request.get(self.__url, params={'value': self.__decrypted_HTTPS_response, 'mode': 'DOWNLOAD'}, stream=True)
         except Exception as e:
-            print('HTTP request error in Downloader.')
+            print('HTTPS request error in Downloader.')
             print(e)
             sys.exit(1)
-        file_name = Downloader.__get_filename_from_cd(self, HTTP_response.headers.get('content-disposition'))
+        file_name = Downloader.__get_filename_from_cd(self, HTTPS_response.headers.get('content-disposition'))
         #Download with progress bar
         with open(file_name, 'wb') as f:
-            total_length = int(HTTP_response.headers.get('content-length'))
-            for chunk in progress.bar(HTTP_response.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1):
+            total_length = int(HTTPS_response.headers.get('content-length'))
+            for chunk in progress.bar(HTTPS_response.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1):
                 if chunk:
                     f.write(chunk)
                     f.flush()
-        del HTTP_response        
-            
+        del HTTPS_response
+
     #Getting the file name of the downloading file from header "content-disposition".
     def __get_filename_from_cd(self, cd):
         file_name = re.findall('filename="(.+)"', cd)
